d4d8878a6b3e6d1dbef77cf4d1c26a957bf19d78
Trmrskr <simonalale@gmail.com>
2023-07-06 23:33:40 +0000

    Annotated sum of list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Complex list types
 """
@@ -8,4 +8,4 @@ from typing import Callable, Iterator, Union, Optional, List
 def sum_list(input_list: List[float]) -> float:
     """Sum of list value"""
 
-    return sum(input_list);
+    return sum(input_list)
