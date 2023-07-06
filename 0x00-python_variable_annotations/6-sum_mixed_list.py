77cfe2fb0ae364d9e47b16a025497d6a6fc3990a
Trmrskr <simonalale@gmail.com>
2023-07-06 23:38:34 +0000

    Sum mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+  Complex types - mixed list
+"""
+
+
+def sum_mixed_list(mxd_lst: float[int | float]) -> float:
+    """
+    Sum a mixed list
+    """
+
+    return sum(mxd_lst)
