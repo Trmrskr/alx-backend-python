9785d616fb61d99c5aee2519c6ab7fd31a46bf26
Trmrskr <simonalale@gmail.com>
2023-08-05 09:57:38 -0700

    Minor correction

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from utils import (access_nested_map, get_json, memoize)
 from parameterized import parameterized
+import unittest
 
 
 class TestAccessNestedMap(unittest.TestCase):
